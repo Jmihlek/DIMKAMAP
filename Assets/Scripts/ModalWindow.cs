1f5e299d5772eb2689a7d5b64d2b7bbd4a825f92
Ternovoy Dmitriy <mioakiyama245@gmail.com>
2024-12-19 22:59:37 +1000

    Воспроизведение звуки в модальных окнах

--- Assets/Scripts/ModalWindow.cs
+++ Assets/Scripts/ModalWindow.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(AudioSource))]
 public class ModalWindow : MonoBehaviour
 {
     public UnityEvent EventAfterClose;
     public DelayedUnityEvent DelayedEventAfterClose;
     public static bool IsShowModalNow = false;
     private PlayerInput _input;
+    private AudioSource _audioSource;
 
     private void OnEnable()
     {
+        _audioSource = GetComponent<AudioSource>();
+        if (_audioSource && _audioSource.clip != null)
+            _audioSource.Play();
+
         IsShowModalNow = true;
         _input = FindAnyObjectByType<PlayerInput>();
         _input.SwitchCurrentActionMap("Modal");
